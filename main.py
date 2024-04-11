b8ec85f09f01e69e1bcccbf109893a2ba155fd2f
Arkeya <arnabsingha2002@gmail.com>
2024-04-11 18:39:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(debug=True)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/login")
+async def login(username: str, password: str):
+    # Your login logic here
+    return {"message": "Login successful"}
