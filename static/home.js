bdcfc26becdace67c751bdd7b54a2b188a46adbc
Suryathej77 <suryathejsurya@gmail.com>
2024-05-15 10:52:03 +0530

    images, html, css, js

--- static/home.js
+++ static/home.js
@@ -14,6 +14,29 @@ hamburgerBtn.addEventListener("click", () => {
 // Hide mobile menu
 hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
 
+const slider = document.querySelector('.slider');
+const images = Array.from(document.querySelectorAll('.slider img'));
+let currentIndex = 0;
+
+function slideImages() {
+    const imageWidth = images[0].clientWidth;
+    currentIndex = (currentIndex + 1) % images.length;
+    const offset = -currentIndex * imageWidth;
+    slider.style.transition = 'transform 0.8s ease-in-out';
+    slider.style.transform = `translateX(${offset}px)`;
+}
+
+setInterval(slideImages, 3000);
+
+
+
+
+
+// Redirect links
+const aboutUsLink = document.querySelector(".links a[href='/about']");
+const rateLink = document.querySelector(".links a[href='/rate']");
+const homeLink = document.querySelector(".links a[href='/home']");
+const ratedLink = document.querySelector(".links a[href='/rated']");
 
 aboutUsLink.addEventListener("click", () => {
     window.location.href = "/about-us";
@@ -29,8 +52,4 @@ homeLink.addEventListener("click", () => {
 
 ratedLink.addEventListener("click", () => {
     window.location.href = "/rated";
-});
-
-aboutUsLink.addEventListener("click", () => {
-    window.location.href = "/dining";
 });
